8f136bfd17923e92312c06c01ac24fcc213f77eb
Oleksandr Chernomorets <achernomorez@gmail.com>
2022-12-07 00:02:46 +0300

    color-swiich

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startEl = document.querySelector('button[data-start]');
+const stopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+let bgcTimer = null;
+
+startEl.addEventListener('click', onChangeColor);
+stopEl.addEventListener('click', offChangeColor);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function onChangeColor() {
+    startEl.setAttribute('disabled', 'true');
+    stopEl.removeAttribute('disabled');
+    
+    bgcTimer = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+   
+}
+
+function offChangeColor() {
+    stopEl.setAttribute('disabled', 'true');
+    startEl.removeAttribute('disabled');
+    clearInterval(bgcTimer);
+    
+}
+
 
